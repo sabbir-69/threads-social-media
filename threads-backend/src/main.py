e3bfe9c6685ab61960d5900310e6d6d0097acd73
sabbir <sabbir@Sabbirs-Mac-mini.local>
2025-06-21 23:39:39 +0600

    Configure database URI to use environment variable

--- threads-backend/src/main.py
+++ threads-backend/src/main.py
@@ -17,7 +17,7 @@ CORS(app, origins=['*'])
 app.register_blueprint(user_bp, url_prefix='/api')
 
 # Database configuration
-app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(os.path.dirname(__file__), 'database', 'app.db')}"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
